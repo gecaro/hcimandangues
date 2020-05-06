296a243e4a4ade601fa525f45e725556b02028c9
ArnauC <51742717+ArnauColom@users.noreply.github.com>
2020-05-06 20:18:52 +0200

    creckestoyasta

--- det_landmarks.py
+++ det_landmarks.py
@@ -1,16 +1,60 @@
 import cv2
 import dlib
+import math 
 
+
+
+
+def calculateDistance(x1,y1,x2,y2):  
+     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  
+     return dist  
 
 def set_landmarks(img_gray, img, face, predictor, detector):
     landmarks = predictor(img_gray, face)
     landmarks_list = []
     for i in range(0, landmarks.num_parts):
         landmarks_list.append((landmarks.part(i).x, landmarks.part(i).y))
+
+
     # for each landmark, plot and write number
+
+    ##Jump rñhjslvhpdvslghslgslh
+    factor_j = 0
+    jump = False
+    distancia_jump = calculateDistance(landmarks.part(41).x,landmarks.part(41).y,landmarks.part(19).x,landmarks.part(19).y)   
+
+
+    ##fireball borjborbhrobhrohb
+    factor_f = 0
+    fireball = False
+    distanica_fireball= calculateDistance(landmarks.part(51).x,landmarks.part(51).y,landmarks.part(57).x,landmarks.part(57).y) 
+
+    ##shield
+    factor_s = 0
+    shield = False
+    distancia_shield = calculateDistance(landmarks.part(48).x,landmarks.part(48).y,landmarks.part(54).x,landmarks.part(54).y) 
+
+
+    if distancia_jump>35.0:
+       #print("JUMP");
+        jump = True
+        factor_j = 1
+
+    elif distanica_fireball>32.0:
+       #print("FIREBALL");
+        fireball = True
+        factor_f = 1
+
+    elif distancia_shield>65.0:
+        #print("SHIELD");                                      
+        shield = True
+        factor_s = 1    
+
+
     for landmark_num, xy in enumerate(landmarks_list, start = 1):
-        cv2.circle(img, (xy[0], xy[1]), 12, (168, 0, 20), -1)
+        cv2.circle(img, (xy[0], xy[1]), 5, (100*factor_j, 100*factor_s, 100*factor_f), -1)
         cv2.putText(img, str(landmark_num),(xy[0]-7,xy[1]+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(255,255,255), 1)
+
 
 def face_detection(img, predictor, detector):
     # convert to grayscale
